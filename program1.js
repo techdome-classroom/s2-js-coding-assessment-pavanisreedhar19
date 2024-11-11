b4172a17ed8a9eca9e325cb7f85f9cd53268e4e7
pavanisreedhar19 <pavanisreedhar1908@gmail.com>
2024-11-11 15:24:26 +0530

    solved

--- program1.js
+++ program1.js
@@ -1,11 +1,29 @@
-/**
- * @param {string} s
- * @return {boolean}
- */
 var isValid = function(s) {
+    // Map for matching closing brackets
+    const matchingBracket = {
+        '(': ')',
+        '{': '}',
+        '[': ']'
+    };
     
+    let stack = [];
+
+    for (let char of s) {
+        if (matchingBracket[char]) {
+            // If it's an opening bracket, push onto the stack
+            stack.push(char);
+        } else {
+            // If it's a closing bracket, check the stack
+            const lastOpen = stack.pop();
+            if (matchingBracket[lastOpen] !== char) {
+                // If it doesn't match the expected closing bracket
+                return false;
+            }
+        }
+    }
+
+    // If the stack is empty, all brackets are matched
+    return stack.length === 0;
 };
 
 module.exports = { isValid };
-
-
--- program2.js
+++ program2.js
@@ -1,10 +1,31 @@
-/**
- * @param {string} s
- * @return {number}
- */
 var romanToInt = function(s) {
+    // Map each Roman numeral character to its corresponding integer value
+    const romanMap = {
+        'I': 1,
+        'V': 5,
+        'X': 10,
+        'L': 50,
+        'C': 100,
+        'D': 500,
+        'M': 1000
+    };
+
+    let result = 0;
+
     
+    for (let i = 0; i < s.length; i++) {
+        const current = romanMap[s[i]];    
+        const next = romanMap[s[i + 1]];   // Next Roman numeral value (or undefined if it's the last character)
+
+        // If the current value is less than the next value, subtract the current value
+        if (current < next) {
+            result -= current;
+        } else {
+            result += current;
+        }
+    }
+
+    return result;
 };
 
-
-module.exports={romanToInt}
+module.exports = { romanToInt };
